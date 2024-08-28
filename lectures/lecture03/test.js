48d65d5d852f7fe278de174d6ea7cab75899c6fb
Brendan <bcasey1@unca.edu>
2024-08-28 14:55:13 -0400

    Activity 3b Commit

---
+++ lectures/lecture03/test.js
@@ -0,0 +1,8 @@
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+const sum1 = addNumbers(9, 10);
+console.log(sum1);
+
+console.log(addNumbers(10, 90));
---
+++ lectures/lecture04/README.md
@@ -0,0 +1,1 @@
+# Fake website files for practice activity
